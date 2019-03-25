0229484b378cdd1cffb0ea283c350433cb16b6c6
nivhack <niv.assassin@gmail.com>
2019-03-25 21:21:48 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import './App.css';
+
+import pikaSrc from './pika.png';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={pikaSrc} className="App-logo" alt="logo" />
+          <h1 className="App-title">uv birthday project</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
